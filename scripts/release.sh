91dd13ddc9b22ed0a8b55cb89eae432e1c150b5b
Andrew Wei <andrew@variante.io>
2016-03-12 02:50:16 -0800

    Fix release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -23,8 +23,8 @@ mv docs/* .
 rm -R docs/
 
 # Push to gh-pages.
-git config user.name "$USER_NAME"
-git config user.email "$USER_EMAIL"
+git config user.name "$GITHUB_USERNAME"
+git config user.email "$GITHUB_EMAIL"
 
 git add -fA
 git commit --allow-empty -m "$(git log -1 --pretty=%B) [ci skip]"
@@ -34,14 +34,13 @@ echo "Done publishing docs, now publishing to npm..."
 
 git checkout -
 
-if [[ -n "$NPM_AUTH" && -n "$NPM_EMAIL" ]]; then
-  echo "_auth=$NPM_AUTH" >> ~/.npmrc
-  echo "email=$NPM_EMAIL" >> ~/.npmrc
-
-  npm publish
-  echo "Done"
+if [[ -n "$NPM_AUTH" ]]; then
+  echo "//registry.npmjs.org/:_authToken=$NPM_AUTH" >> ~/.npmrc
+  run npm publish
 else
-  echo "'npm publish' was skipped because NPM_AUTH and NPM_EMAIL are not set." >&2
+  echo "'npm publish' was skipped because NPM_AUTH is not set." >&2
 fi
+
+echo "Done"
 
 exit 0
